9545825e227b36ab4997b61710ec20d5ef8e01b8
Samuel Ntewusu <sammyntewusu1@gmail.com>
2023-09-11 13:52:29 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/">
+    <Route index element={<Home />} />
+  </Route>
+))
+
+function App() {
+  return <RouterProvider router={router} />
+  
+}
+
+export default App
