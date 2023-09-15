75ddc2f9e155c1e13b928041359af887f9c1c962
Samuel Ntewusu <sammyntewusu1@gmail.com>
2023-09-15 21:04:59 +0000

    fix 1.1

--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import SearchSvg from "./Svgs/SearchSvg";
+import SearchSvg from "./svgs/SearchSvg";
 import { Link } from "react-router-dom";
 
 export default function SearchArea() {
